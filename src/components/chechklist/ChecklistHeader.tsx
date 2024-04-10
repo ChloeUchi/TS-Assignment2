640f5c414db06ec2016cc7527836fae320e34cb2
ChloeUchi <chonthicha.hi@kkumail.com>
2024-04-10 15:16:59 +0700

    [EDIT]:Edit tailwind css

--- src/components/chechklist/ChecklistHeader.tsx
+++ src/components/chechklist/ChecklistHeader.tsx
@@ -5,9 +5,9 @@ const ChecklistHeader: React.FC = () => {
   return (
     <>
     <div className='flex-container'>
-      <div className='div-style'>คำศัพท์</div>
-      <div className='div-style'>ภาษาไทย</div>
-      <div className='div-style'>ภาษาอังกฤษ</div>
+      <div className='div-style border-4 border-[#54517e] font-bold'>คำศัพท์</div>
+      <div className='div-style border-4 border-[#54517e] font-bold'>ภาษาไทย</div>
+      <div className='div-style border-4 border-[#54517e] font-bold'>ภาษาอังกฤษ</div>
     </div>
     </>
   );
--- src/components/chechklist/ChecklistItem.tsx
+++ src/components/chechklist/ChecklistItem.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { CiLock, CiUnlock } from "react-icons/ci";
+// import { FiClock } from "react-icons/fi";
 
 interface Props {
   checked: boolean;
   word: {
+    timestamp: number;
     word: string;
     locked?: boolean;
   };
@@ -22,20 +24,37 @@ const ChecklistItem: React.FC<Props> = ({ word, onClick, checked, onLock }) => {
     onLock();
   };
 
+    const calculateRemainingTime = (timestamp: number) => {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - timestamp;
+      const remainingTime = Math.max(0, 5000 - elapsedTime); // Limit to 0 if exceeded 5 seconds
+      return Math.ceil(remainingTime / 1000); // Convert milliseconds to seconds and round up
+    };
+    
   return (
     <>
       {checked ? 
       (
         <div>
-          <div className="word-button">
-            <button onClick={handleClick} className="pr-[5em]">{word.word} </button>
+          <div className="word-button flex justify-between border-2 border-[#d4d1f5] hover:border-[#9597c1]">
+            <div className="flex justify-between">
+            <button onClick={handleClick} className="pl-[7em]">{word.word} </button>
             {word.locked ? (
-              <button onClick={handleLock}><CiLock /></button>
+              <button onClick={handleLock} className="ml-[1em] mr-[2em]" ><CiLock /></button>
               // <i onClick={handleLock}><CiLock /></i>
             ) : (
-              <button onClick={handleLock}><CiUnlock /></button>
+              <button onClick={handleLock} className="ml-[1em] mr-[2em]"><CiUnlock /></button>
               // <i onClick={handleLock}><CiUnlock /></i>
             )}
+            </div>
+            <div>
+            {word.timestamp > 0 && !word.locked && ( // Show countdown timer only if word is not locked
+            <div className="timer-icon  self-center text-sm mt-[1.9em] mr-[1em] text-[#ff4444]">
+              {/* <FiClock />  */}
+              {calculateRemainingTime(word.timestamp)}
+            </div>
+          )}
+          </div>
           </div>
           
         </div>
